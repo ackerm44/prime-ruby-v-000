8aed293d725a513442c7bdb3fbeb240a8226dbe3
Amanda Ackerman <ackerm44@gmail.com>
2017-10-24 03:26:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,6 @@
 def prime?(int)
   collection = (2..100).to_a
   collection.delete_if {|i| i == int}
-  puts collection.detect {|i| int % i == 0 and int > 0}
-  #puts collection.all? {|i| int % i == 0 and int > 0}
+  puts collection.all? {|i| int % i == 0 and int > 0}
 
 end
